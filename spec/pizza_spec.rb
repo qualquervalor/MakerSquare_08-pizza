5bdccc887f4ba8a7a574a8a4d626a7f1586c327a
Qualquer Valor <qualquervalor@gmail.com>
2013-11-20 10:59:53 -0600

    added a fail test for Pizza::Topping.initialize

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -8,5 +8,11 @@ describe Pizza::Pie do
 end 
 
 describe Pizza::Topping do
+	describe '.initialize' do
+			it "sets the name of the topping" do
+				topping = Pizza::Topping.new('mushrooms')
 
+				expect(topping.name).to_eq('mushrooms')
+			end
+	end
 end
