c86d18c19187ab70cbd463cdf5b0c3e30b9a339c
Qualquer Valor <qualquervalor@gmail.com>
2013-11-20 23:46:38 -0600

    This is a sample example of using rspec to test object creation

--- pizza.rb
+++ pizza.rb
@@ -1,9 +1,34 @@
+# Various classes about Pizzas and their ingredients
 module Pizza
-  class Pie
 
+	#This is a Pizza Pie had has an array of Topping
+  class Pie
+  	attr_accessor :toppings
+
+	  # Instantiate a new pizza pie.
+		#
+		# toppings   - The array of topping.
+		#
+		# Returns the new Pie.
+  	def initialize(toppings = [Pizza::Topping.new('cheese',vegetarian: true)])
+  		@toppings = toppings
+  	end
   end
 
   class Topping
+  	attr_accessor :name, :vegetarian
 
+	  # Instantiate a new topping.
+		#
+		# name       - The String name of the topping.
+		# vegetarian - The Boolean indicating whether or not the topping is
+		#              vegetarian.
+		#
+		# Returns the new Topping.
+		def initialize(name, vegetarian: false)
+		  @name = name
+		  @vegetarian = vegetarian
+		end
   end
+
 end
--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -4,15 +4,40 @@ require 'rspec'
 require_relative '../pizza'
 
 describe Pizza::Pie do
+  describe '.initialize' do
+    it 'records all of the toppings' do
+      toppings = [
+        Pizza::Topping.new('mushrooms', vegetarian: true),
+        Pizza::Topping.new('pepperoni')
+      ]
+      pizza = Pizza::Pie.new(toppings)
 
+      expect(pizza.toppings).to eq(toppings)
+    end
+  end
+  describe '.initialize' do
+    it 'defaults the toppings to cheese only, if the pizza has no toppings' do
+      pizza = Pizza::Pie.new
+
+      expect(pizza.toppings.size).to eq(1)
+      expect(pizza.toppings.first.name).to eq('cheese')
+    end
+  end
 end 
 
 describe Pizza::Topping do
-	describe '.initialize' do
-			it "sets the name of the topping" do
-				topping = Pizza::Topping.new('mushrooms')
+    describe '.initialize' do
+            it "sets the name of the topping" do
+                topping = Pizza::Topping.new('mushrooms')
 
-				expect(topping.name).to_eq('mushrooms')
-			end
-	end
+                expect(topping.name).to eq('mushrooms')
+            end
+    end
+    describe '.initialize' do 
+      it 'sets whether or not the topping is vegetarian' do
+        topping = Pizza::Topping.new 'bell peppers', vegetarian: true
+
+        expect(topping.vegetarian).to eq(true)
+      end
+    end
 end
